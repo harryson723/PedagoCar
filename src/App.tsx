324109f3e2a5ab685ab2544408808ee54cf3cef2
TuNombre <harrysonleal17@gmail.com>
2024-05-26 11:18:29 -0500

    hola

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,6 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -53,13 +50,13 @@ const App: React.FC = () => (
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/tab1">
-            <Tab1 />
+           
           </Route>
           <Route exact path="/tab2">
-            <Tab2 />
+          
           </Route>
           <Route path="/tab3">
-            <Tab3 />
+           
           </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
